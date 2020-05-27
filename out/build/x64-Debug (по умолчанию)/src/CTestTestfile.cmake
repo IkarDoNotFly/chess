382025058a40c3d5e0d6bde7c59f920cee14906e
IkarDoNotFly <47333373+IkarDoNotFly@users.noreply.github.com>
2020-05-28 01:45:44 +0300

    138

--- out/build/x64-Debug (по умолчанию)/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Вячеслав/Desktop/chess123/chess/src
-# Build directory: C:/Users/Вячеслав/Desktop/chess123/chess/out/build/x64-Debug (по умолчанию)/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug (по умолчанию)/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Вячеслав/Desktop/chess123/chess/tests
-# Build directory: C:/Users/Вячеслав/Desktop/chess123/chess/out/build/x64-Debug (по умолчанию)/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
